ef18517e3622d1f784cede85f63bac4c826a2979
Ashfaq Ahmed <ashfaqdotes@gmail.com>
2024-08-17 14:48:14 +0500

    files upload

---
+++ firebase.mjs
@@ -0,0 +1,18 @@
+// Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXq6muaGUMnY71-Gs8FjFTM35ZNv_zJm0",
+  authDomain: "blog-web-c757e.firebaseapp.com",
+  projectId: "blog-web-c757e",
+  storageBucket: "blog-web-c757e.appspot.com",
+  messagingSenderId: "575023728478",
+  appId: "1:575023728478:web:71bbcb9b4bce3adac5b1cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
