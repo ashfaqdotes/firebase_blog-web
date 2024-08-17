b1f043d67f2139dfb1b078901b53d6050f412b98
Ashfaq Ahmed <ashfaqdotes@gmail.com>
2024-08-17 16:48:32 +0500

    issue firebase

--- firebase.mjs
+++ firebase.mjs
@@ -1,6 +1,7 @@
 // Firebase
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,3 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {app,auth}
+
+// console.log('Firebase Initialize:-',app);
